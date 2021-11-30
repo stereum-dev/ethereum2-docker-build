a3884195c481aa46adda01e6fa61e1c9842b0dac
root <bayasgalan.gantumur@rocklogic.at>
2021-11-30 13:42:34 +0100

    add trace & verbose for testing purpose

--- lighthouse/start/import-launchpad-wallet.sh
+++ lighthouse/start/import-launchpad-wallet.sh
@@ -6,7 +6,7 @@ set -o verbose
 opt_app_lighthouse="/opt/app/lighthouse"
 account_validator_import="account validator import" 
 
-echo $opt_app_lighthouse | bash \
+echo $opt_app_lighthouse \
      --debug-level "$DEBUG_LEVEL" \
      --network "$NETWORK" \
      echo $account_validator_import | bash \
