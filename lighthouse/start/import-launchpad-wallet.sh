17f302747c170c57d85917fd9c32b6135ca6b8d4
root <bayasgalan.gantumur@rocklogic.at>
2021-11-30 13:44:50 +0100

    add trace & verbose for testing purpose

--- lighthouse/start/import-launchpad-wallet.sh
+++ lighthouse/start/import-launchpad-wallet.sh
@@ -3,12 +3,11 @@
 set -o xtrace
 set -o verbose
 
-opt_app_lighthouse="/opt/app/lighthouse"
 account_validator_import="account validator import" 
 
-echo $opt_app_lighthouse \
+/opt/app/lighthouse \
      --debug-level "$DEBUG_LEVEL" \
      --network "$NETWORK" \
-     echo $account_validator_import | bash \
+     echo $account_validator_import \
      --datadir "$DATADIR" \
      --directory "$LAUNCHPADDIR"
