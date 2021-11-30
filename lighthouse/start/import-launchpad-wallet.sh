7c948366182ce30729de405581a02d3aa7504254
root <bayasgalan.gantumur@rocklogic.at>
2021-11-30 14:14:11 +0100

    debug

--- lighthouse/start/import-launchpad-wallet.sh
+++ lighthouse/start/import-launchpad-wallet.sh
@@ -4,7 +4,7 @@ set -o xtrace
 set -o verbose 
 
 /opt/app/lighthouse \
-     --debug-level debug \
+     --debug-level "debug" \
      --network "$NETWORK" \
      account validator import \
      --datadir "$DATADIR" \
